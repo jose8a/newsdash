6f8f01c70d6f5572e3fe8878d5053840374a01bd
jose8a <ochovilla@gmail.com>
2018-01-07 15:46:06 -0800

    Update links to genre and sources components with a dummy genre and source parameters, respectively.

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -2,8 +2,8 @@
     <div id='nav'>
       <router-link to='/' tag="button">Home</router-link>
       <router-link to='/favorites' tag="button">Favorites</router-link>
-      <router-link to='/genres/:id' tag="button">Genres</router-link>
-      <router-link to='/sources/:id' tag="button">Sources</router-link>
+      <router-link to='/genres/tbd' tag="button">Genres</router-link>
+      <router-link to='/sources/tbd' tag="button">Sources</router-link>
     </div>
 </template>
 
