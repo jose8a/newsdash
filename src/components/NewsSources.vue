71a324078375dec19b6c0f9e81f92f668499aaf5
jose8a <ochovilla@gmail.com>
2018-01-10 23:31:00 -0800

    Update NewsSources component to display a collection of newsItems based on the NewsSources url-target requested.

--- src/components/NewsSources.vue
+++ src/components/NewsSources.vue
@@ -1,6 +1,12 @@
 <template>
     <div id='news-sources'>
       <h2>News Sources</h2>
+      <ul>
+        <li v-for="item in newsItems" :id="item.sourceId" class="postItem" :class="item.source">
+          <p><a :href="item.url">{{ item.title }}</a></p>
+          <p class=""> {{ postHost(item.url) }}</p>
+        </li>
+      </ul>
     </div><!-- end:#news-sources -->
 </template>
 
@@ -8,17 +14,50 @@
 /* eslint-disable no-console */
 /* eslint-disable object-shorthand */
 
+import axios from 'axios';
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'sources',
   data() {
     return {
+      server: null,
+      newsItems: [],
     };
   },
   mounted() {
+    const source = this.$route.params.source;
+    const sourceType = this.$route.params.type;
+    const newsUrl = `/${sourceType}/${source}`;
+
+    this.server = this.http();
+    this.server.get(newsUrl)
+      .then((resp) => {
+        console.log(`AXIOS RESP: ${resp.data}`);
+        this.newsItems = resp.data;
+      })
+      .catch((err) => {
+        // TODO: error ACTION to display error banner
+        console.log(`AXIOS ERR: ${err}`);
+      });
   },
-  computed: {
-  },
+  computed: mapGetters({
+    serverUrl: 'getBaseUrl',
+  }),
   methods: {
+    http() {
+      console.log(`http creation -- baseUrl: ${this.serverUrl}`);
+      return axios.create({
+        baseURL: this.serverUrl,
+        responseType: 'json',
+      });
+    },
+    postHost(url) {
+      return url.split('/')[2];
+    },
+    news() {
+      return this.newsItems;
+    },
   },
 };
 </script>
