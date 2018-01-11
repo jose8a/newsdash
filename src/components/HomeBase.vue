5cb7d2162432b75a20729216c6a785d31a632c51
jose8a <ochovilla@gmail.com>
2018-01-10 23:28:41 -0800

    Update HomeBase component to show links for available News Sources.

--- src/components/HomeBase.vue
+++ src/components/HomeBase.vue
@@ -1,11 +1,16 @@
 <template>
     <div id='home'>
       <h2>I am HOME!</h2>
+      <div v-for="source in sourceKeys">
+        <a :href="endpoint(source)"> {{ apiSources[source].title }} </a>
+      </div>
     </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
+
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'home',
@@ -14,6 +19,19 @@ export default {
     };
   },
   mounted() {
+  },
+  computed: {
+    ...mapGetters({
+      serverUrl: 'getBaseUrl',
+      apiSources: 'getSources',
+      sourceKeys: 'getSourceKeys',
+    }),
+    endpoint() {
+      return (src) => {
+        const apiSource = this.apiSources[src];
+        return `/sources${apiSource.endpoint}`;
+      };
+    },
   },
   methods: {
   },
