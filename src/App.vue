97d838acd23cc19fe6057d255cb1cb51f7ffd7af
jose8a <ochovilla@gmail.com>
2018-01-14 23:53:19 -0800

    Add the footer component to the App.  Update the path for the NavHeader component.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <nav-header></nav-header>
     <router-view></router-view>
+    <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
-import NavHeader from './components/NavHeader';
+import NavHeader from '@/components/NavHeader';
+import NavFooter from '@/components/Footer';
 
 export default {
   name: 'app',
   components: {
     NavHeader,
+    NavFooter,
   },
   computed: {
   },
