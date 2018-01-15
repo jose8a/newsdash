20b6fda674955cc1604713eb65579fa92fc5190e
jose8a <ochovilla@gmail.com>
2018-01-14 23:50:30 -0800

    Create footer element to add some information at bottom of newsDash site.

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav-footer">
+    <div>Site is handcrafted by Juan</div>
+    <a href="https://github.com/jose8a/newsdash">FE Client - GH Repo</a>
+    <a href="https://github.com/jose8a/newshound">API Server - GH Repo</a>
+    <div id="social">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nav-footer',
+  };
+</script>
