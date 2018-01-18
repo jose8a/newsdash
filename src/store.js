375eea50bd97d0546d35d9cc6e72b5372bbfe39e
jose8a <ochovilla@gmail.com>
2018-01-18 01:17:26 -0800

    Update storage containers for easier querying of already-fetched news items.

--- src/store.js
+++ src/store.js
@@ -25,96 +25,87 @@ export default new Vuex.Store({
       data: [],
     },
 
+    items: {
+      favorites: {},
+      bydate: {},
+      all: {},
+    },
+
     newsSources: {
       hnews: {
         title: 'Hacker News',
         url: 'https://news.ycombinator.com/',
         endpoint: '/motley/hnews',
-        data: [],
       },
       reuters: {
         title: 'Reuters',
         url: 'https://www.reuters.com/',
         endpoint: '/motley/reuters',
-        data: [],
       },
       topMedium: {
         title: 'Medium - Top',
         url: 'https://medium.com/browse/top',
         endpoint: '/motley/top-medium',
-        data: [],
       },
       theRinger: {
         title: 'The Ringer',
         url: 'https://www.theringer.com/',
         endpoint: '/sports/the-ringer',
-        data: [],
       },
       siCom: {
         title: 'Sports Illustrated',
         url: 'https://www.si.com/',
         endpoint: '/sports/si-com',
-        data: [],
       },
       sbnation: {
         title: 'SB Nation',
         url: 'https://www.sbnation.com/',
         endpoint: '/sports/sbnation',
-        data: [],
       },
       fefront: {
         title: 'FE Front',
         url: 'https://frontendfront.com/',
         endpoint: '/tech/fefront',
-        data: [],
       },
       echojs: {
         title: 'Echo JS',
         url: 'http://www.echojs.com/',
         endpoint: '/tech/echo',
-        data: [],
       },
       cssTricks: {
         title: 'CSS Tricks',
         url: 'https://css-tricks.com/',
         endpoint: '/tech/css-tricks',
-        data: [],
       },
       devTo: {
         title: 'DEV Comm',
         url: 'https://dev.to/',
         endpoint: '/tech/dev-to',
-        data: [],
       },
       scotch: {
         title: 'Scotch Devs',
         url: 'https://scotch.io/',
         endpoint: '/tech/scotch',
-        data: [],
       },
       perfRocks: {
         title: 'Perf Rocks',
         url: 'http://perf.rocks/articles/',
         endpoint: '/tech/perf-rocks',
-        data: [],
       },
       vuedevs: {
         title: 'Vue Devs',
         url: 'https://vuejsdevelopers.com/',
         endpoint: '/vue/vuedevs',
-        data: [],
       },
       gator: {
         title: 'Vue Gator',
         url: 'https://alligator.io/vuejs/',
         endpoint: '/vue/gator',
-        data: [],
       },
       reddit: {
         title: 'Vue Reddit',
         url: 'https://www.reddit.com/r/vuejs/',
         endpoint: '/vue/reddit',
-        data: [],
       },
     },
   },
