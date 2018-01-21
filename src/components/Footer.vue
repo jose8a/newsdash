6fab0256e2f08fbb3d737b70887f5e41ebe0ec01
jose8a <ochovilla@gmail.com>
2018-01-21 15:06:07 -0800

    Add GH icons to footer resource links. Improve layout for footer elements.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="nav-footer">
     <div>Site is handcrafted by Juan</div>
-    <a href="https://github.com/jose8a/newsdash">FE Client - GH Repo</a>
-    <a href="https://github.com/jose8a/newshound">API Server - GH Repo</a>
+    <a href="https://github.com/jose8a/newsdash">
+      <i class="fa fa-github"></i> SPA
+    </a>
+    <a href="https://github.com/jose8a/newshound">
+      <i class="fa fa-github"></i> API
+    </a>
     <div id="social">
     </div>
   </div>
