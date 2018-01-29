ddebb4311858e80d19991461fd745fa71456052b
jose8a <ochovilla@gmail.com>
2018-01-29 04:10:05 -0800

    Improve checking for an empty object - solution borrowed from Stack Overflow.

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,38 @@
 /* eslint-disable quotes */
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
+
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-prototype-builtins */
+function isEmpty(obj) {
+  // CREDIT:
+  // https://stackoverflow.com/questions/4994201/is-object-empty
+  //
+  // null and undefined are "empty"
+  if (obj == null) return true;
+
+  // Assume if it has a length property with a non-zero value
+  // that that property is correct.
+  if (obj.length > 0) return false;
+  if (obj.length === 0) return true;
+
+  // If it isn't an object at this point
+  // it is empty, but it can't be anything *but* empty
+  // Is it empty?  Depends on your application.
+  if (typeof obj !== "object") return true;
+
+  // Otherwise, does it have any properties of its own?
+  // Note that this doesn't handle
+  // toString and valueOf enumeration bugs in IE < 9
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 export default {
   getBaseUrl(state) {
     return state.baseServerUrl;
@@ -26,7 +58,7 @@ export default {
     const allItems = state.dataStores.all;
 
     let faveKeys = null;
-    if (favorites !== null) {
+    if (!isEmpty(favorites)) {
       faveKeys = Object.keys(favorites);
 
       faveKeys.forEach((id) => {
@@ -39,7 +71,7 @@ export default {
   getNumFavorites(state) {
     const favorites = state.dataStores.favorites;
 
-    if (favorites !== null) {
+    if (!isEmpty(favorites)) {
       const faveKeys = Object.keys(state.dataStores.favorites);
       return faveKeys.length;
     }
@@ -54,7 +86,7 @@ export default {
     const allItems = state.dataStores.all;
 
     let bmIds = null;
-    if (bookmarkIds !== null) {
+    if (!isEmpty(bookmarkIds)) {
       bmIds = Object.keys(bookmarkIds);
 
       bmIds.forEach((id) => {
@@ -67,7 +99,7 @@ export default {
   getNumBookmarks(state) {
     const bookmarkIds = state.dataStores.bookmarks;
 
-    if (bookmarkIds !== null) {
+    if (!isEmpty(bookmarkIds)) {
       const objKeys = Object.keys(state.dataStores.bookmarks);
       return objKeys.length;
     }
