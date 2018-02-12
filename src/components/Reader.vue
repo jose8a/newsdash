c4da7a724171536d7272f7e6eac2c723c7ce57fa
jose8a <ochovilla@gmail.com>
2018-02-11 18:15:07 -0800

    Show the Bookmarks feed sorted by date, ascending.

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -11,7 +11,7 @@
         <p class="post-url">You have Zero Bookmarked items on your Shelf.</p>
       </div>
 
-      <news-item v-for="item in bookmarks" :newsbit="item" :key="item.sourceId"></news-item>
+      <news-item v-for="item in sortedBookmarks" :newsbit="item" :key="item.sourceId"></news-item>
     </div><!-- end:#newsfeed -->
 </template>
 
@@ -34,6 +34,21 @@ export default {
     }),
     nilBookmarkItems() {
       return this.numBookmarks === 0;
+    },
+    sortedBookmarks() {
+      const entries = this.bookmarks;
+
+      return entries.sort((entry1, entry2) => {
+        if (entry1.fetchDate > entry2.fetchDate) {
+          return -1;
+        }
+
+        if (entry2.fetchDate > entry1.fetchDate) {
+          return 1;
+        }
+
+        return 0;
+      });
     },
   },
 };
